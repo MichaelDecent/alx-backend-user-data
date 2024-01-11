a450173e162e84143a11215f0f9584485d0ff628
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-11 23:27:53 +0100

    fixed error

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,4 +10,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validates an hashed password and returns a boolean"""
-    return bcrypt.checkpw(b"password", hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
