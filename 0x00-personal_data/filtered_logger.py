2860b9f9685cc1a7a149c144282ddd61e8da8ac6
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-11 15:28:54 +0100

    fix pycodestyle error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -60,6 +60,6 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
         user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
         password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-        database=os.getenv("PERSONAL_DATA_DB_NAME"),
+        database=os.getenv("PERSONAL_DATA_DB_NAME", ""),
     )
     return db_connection
