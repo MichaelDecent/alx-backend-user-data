9e17280bfe7fd9a6ae5b93420371bcba8b59a2e9
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-11 23:17:51 +0100

    added solution to task 5 & 6

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This Module contains a function that encrypts a password"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """This function returns an hashed password"""
+    hashed = bcrypt.hashpw(b"password", bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: str, password: str) -> bool:
+    """Validates an hashed password and returns a boolean"""
+    if bcrypt.checkpw(b"password", hashed_password):
+        return True
+    return False
