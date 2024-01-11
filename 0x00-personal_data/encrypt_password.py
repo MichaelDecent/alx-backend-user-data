e56f52f21f22d3ffaf6c2e53c28240840d9bc14d
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-12 00:09:07 +0100

    fixed error

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,9 +5,9 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """This function returns an hashed password"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validates an hashed password and returns a boolean"""
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -55,20 +55,26 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """creates a database connection"""
-    db_connection = mysql.connector.connect(
-        host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
-        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-        database=os.getenv("PERSONAL_DATA_DB_NAME", ""),
+    """Connect to database and return its connector"""
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pass = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+
+    connection = mysql.connector.connect(
+            host=db_host,
+            port=3306,
+            user=db_user,
+            password=db_pass,
+            database=db_name
     )
-    return db_connection
+    return connection
+
 
 def main() -> None:
     """"""
     db_connection = get_db()
     logger = get_logger()
-
 
     with db_connection.cursor() as cursor:
         cursor.excute('SELECT * FROM users;')
