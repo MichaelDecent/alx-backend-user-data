9bab5d273a25919ab5163c5bb3d3bbf65d64b8ba
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-16 13:41:32 +0100

    fixed pycodestyle error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,7 +69,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 def main() -> None:
     """
     obtain a database connection using get_db and retrieve
-    all rows in the users table and display each row 
+    all rows in the users table and display each row
     """
     db_connection = get_db()
     logger = get_logger()
