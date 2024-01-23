8e4ef72913171fdff019ebaba87e911e5190fc22
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-23 13:17:29 +0100

    added solution to task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,18 +5,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from typing import TypeVar
-
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -24,18 +20,16 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
-    def add_user(self, email: str, hashed_paswword: str) -> TypeVar('User'):
-        """adds a new user object and save it to the database
-        """
-        new_user = User(email, hashed_paswword)
-        self.__session.add(new_user)
-        self.__session.commit()
 
-
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """adds a new user object and save it to the database"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(new_user)
+        session.commit()
+        return new_user
