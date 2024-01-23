724f10849bd012a3a6649e11bead7865f0a0f41c
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-23 16:39:10 +0100

    fixed pycodestyle error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,7 +44,7 @@ class DB:
 
         user = self._session.query(User).filter_by(**kwargs).first()
 
-        if user == None:
+        if user is None:
             raise (NoResultFound)
 
         return user
