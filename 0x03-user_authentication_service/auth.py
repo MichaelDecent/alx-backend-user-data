227fdb26a03331ecd5d634b39aa7ae673a0d02cf
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-23 16:22:45 +0100

    fixed pycodestyle error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,29 +1,27 @@
 import bcrypt
 from user import User
+from db import DB
 from sqlalchemy.exc import NoResultFound
 
 
 def _hash_password(password: str):
-    """Takes in a password string arguments and returns bytes
-    """
+    """Takes in a password string arguments and returns bytes"""
     return bcrypt.hashpw(b"password", bcrypt.gensalt())
 
-from db import DB
 
 class Auth:
-    """Auth class to interact with the authentication database.
-    """
+    """Auth class to interact with the authentication database."""
 
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Regitsers a user
-        """
+        """Regitsers a user"""
         try:
             user = self._db.find_user_by(email=email)
             if user:
                 raise ValueError(f"User {user.email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
-            return self._db.add_user(email=email, hashed_password=hashed_password)
+            return self._db.add_user(
+                email=email, hashed_password=hashed_password)
