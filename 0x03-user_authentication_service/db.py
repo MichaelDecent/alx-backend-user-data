05dd79c322d03f09806e3445057b89fbf7f7c24d
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-23 16:34:33 +0100

    fixed pycodestyle error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,7 +35,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs: Any) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """returns the first row found in the users table as
         filtered by the method’s input arguments
         """
