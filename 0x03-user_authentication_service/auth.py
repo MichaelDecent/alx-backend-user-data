1b68c1ae503dff1563c60445934b1ecadcf46dd6
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-24 01:20:02 +0100

    updated solution on task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,11 +6,16 @@ import bcrypt
 from user import User
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
     """Takes in a password string arguments and returns bytes"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+def _generate_uuid() -> str:
+    """return a string representation of a new UUID"""
+    return str(uuid4())
 
 
 class Auth:
@@ -29,3 +34,21 @@ class Auth:
             hashed_password = _hash_password(password)
             return self._db.add_user(
                 email=email, hashed_password=hashed_password)
+        
+    def valid_login(self, email: str, password: str) -> bool:
+        """validated a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
+        except NoResultFound:
+            return False
+        
+    def create_session(self, email: str) -> str:
+        """creates a database session"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None    
