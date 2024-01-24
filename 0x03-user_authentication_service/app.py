37e816cc0a09efed8ef191e26ab74f1338089ec9
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-24 11:41:16 +0100

    added solution ot task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Starta A flask App
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -31,6 +31,15 @@ def register_user():
 
     return jsonify({"email": f"{new_user.email}", "message": "user created"})
 
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Logs in a user"""
+    response = request.form.to_dict()
+
+    if not AUTH.valid_login(response.get("email"), response.get("password")):
+        abort(401)
+
+    return jsonify({"email": f"{response.get('email')}", "message": "logged in"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
